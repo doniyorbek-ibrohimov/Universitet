3070abd7a29441819a6657b8206082a8135963a0
Пользователь <doniyorbek.info@gmail.com>
2025-03-21 14:20:17 +0500

    first commit

---
+++ main/models.py
@@ -0,0 +1,19 @@
+from random import choices
+
+from django.db import models
+
+class Yonalish(models.Model):
+    nom=models.CharField(max_length=50)
+    aktiv=models.CharField(max_length=50)
+class Fan(models.Model):
+    nom=models.CharField(max_length=40)
+    asosiy=models.BooleanField()
+    yonalish=models.ForeignKey(Yonalish,on_delete=models.CASCADE)
+
+class Ustoz(models.Model):
+    ism=models.CharField(max_length=20)
+    yosh=models.IntegerField()
+    jins=models.CharField(max_length=25,choices=(('Erkak','Erkak'),('Ayol','Ayol')))
+    daraja=models.CharField(max_length=25,choices=(('Bakalavr','Bakalavr'),('Magistr','Magistr')))
+    fan=models.ForeignKey(Fan,on_delete=models.CASCADE)
+
